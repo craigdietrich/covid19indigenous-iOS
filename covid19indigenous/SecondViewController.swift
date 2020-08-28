391eac68be4c46f8dae4555f264ac06a8790b42e
Craig Dietrich <craigdietrich@gmail.com>
2020-08-27 19:54:32 -0700

    Initial Commit

---
+++ covid19indigenous/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  covid19indigenous
+//
+//  Created by Craig Dietrich on 8/27/20.
+//  Copyright © 2020 craigdietrich.com. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
