27a5027f56cb16c12c86a4bafb064ca6fd563a66
Craig Dietrich <craigdietrich@gmail.com>
2020-08-28 20:17:23 -0700

    Setup screen horiz-vert orientations

--- covid19indigenous/FirstViewController.swift
+++ covid19indigenous/FirstViewController.swift
@@ -10,11 +10,53 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var iPhoneVerticalContainer: UIView!
+    @IBOutlet weak var iPhoneHoriztonalContainer: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        doLayout()
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(rotated), name: UIDevice.orientationDidChangeNotification, object: nil)
+        
     }
 
+    @objc func rotated() {
+        
+        doLayout()
+        
+    }
+    
+    func doLayout() {
+        
+        var _isIPhone: Bool = true
+        var _isVertical: Bool = true
+        if UIScreen.main.bounds.width > UIScreen.main.bounds.height {
+            _isVertical = false
+        }
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            _isIPhone = false
+        }
+        
+        if (_isIPhone) {
+            if (_isVertical) {
+                iPhoneVerticalContainer.isHidden = false
+                iPhoneHoriztonalContainer.isHidden = true
+            } else {
+                iPhoneVerticalContainer.isHidden = true
+                iPhoneHoriztonalContainer.isHidden = false
+            }
+        } else {
+            if (_isVertical) {
+                
+            } else {
+                
+            }
+        }
+        
+    }
 
 }
 
