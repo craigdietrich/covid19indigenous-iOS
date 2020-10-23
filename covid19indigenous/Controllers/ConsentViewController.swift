6e9fc61be6c9aac1e0b3f2d283d6994a80135e92
Craig Dietrich <craigdietrich@gmail.com>
2020-10-23 12:27:51 -0700

    Texts

--- covid19indigenous/Controllers/ConsentViewController.swift
+++ covid19indigenous/Controllers/ConsentViewController.swift
@@ -8,20 +8,52 @@
 
 import UIKit
 import WebKit
+import SafariServices
 
-class ConsentViewController: UIViewController {
+class ConsentViewController: UIViewController, WKScriptMessageHandler, WKNavigationDelegate {
 
-    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var webViewWrapper: UIView!
+    var webView: WKWebView!
     
     var callbackClosure: (() -> Void)?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let contentController = WKUserContentController();
+        contentController.add(self, name: "buttonAction")
+        let config = WKWebViewConfiguration()
+        config.userContentController = contentController
+        webView = WKWebView(frame: webViewWrapper.bounds, configuration: config)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webViewWrapper.addSubview(webView)
+         
+        webView.leadingAnchor.constraint(equalTo: webViewWrapper.leadingAnchor, constant: 0).isActive = true
+        webView.trailingAnchor.constraint(equalTo: webViewWrapper.trailingAnchor, constant: 0).isActive = true
+        webView.topAnchor.constraint(equalTo: webViewWrapper.topAnchor, constant: 0).isActive = true
+        webView.bottomAnchor.constraint(equalTo: webViewWrapper.bottomAnchor, constant: 0).isActive = true
         webView.isOpaque = false
+
         let htmlFile = Bundle.main.path(forResource: "consent", ofType: "html")
         let html = try? String(contentsOfFile: htmlFile!, encoding: String.Encoding.utf8)
         webView.loadHTMLString(html!, baseURL: Bundle.main.bundleURL)
+        
+    }
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        
+        let generator = UIImpactFeedbackGenerator(style: .light)
+        generator.impactOccurred()
+        guard let response = message.body as? String else { return }
+        switch (response) {
+            default:
+                if let url = URL(string: response) {
+                    let config = SFSafariViewController.Configuration()
+                    config.entersReaderIfAvailable = true
+                    let vc = SFSafariViewController(url: url, configuration: config)
+                    present(vc, animated: true)
+                }
+        }
         
     }
     
---
+++ covid19indigenous/Controllers/SaveAnswersViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SaveAnswersViewController.swift
+//  covid19indigenous
+//
+//  Created by Craig Dietrich on 10/17/20.
+//  Copyright © 2020 craigdietrich.com. All rights reserved.
+//
+
+import UIKit
+
+class SaveAnswersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
