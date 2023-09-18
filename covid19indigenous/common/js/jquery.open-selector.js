f89cc12c92b318f146428d0e95a9f3011552398f
digcur <craigdietrich@live.com>
2023-09-18 13:58:18 -0700

    Video shows while recording

--- covid19indigenous/common/js/jquery.open-selector.js
+++ covid19indigenous/common/js/jquery.open-selector.js
@@ -101,123 +101,91 @@
 	    				}).trigger('click');
 	    				break;
 	    			case 'video':
-	    				/*
-	    				if (jQuery.browser.mobile || is_safari) {
-		    				$next_cell.append('<input type="hidden" name="base64_string" value="" />');
-		    				$next_cell.append('<input type="file" style="display:none;" />');  // TODO: video-specific attribute
-		    				$next_cell.append('<div style="text-center msg"></div>');
-		    				$next_cell.append('<video class="open-video" controls="" autoplay="" style="width:400px;height:250px;"></video>');
-		    				$next_cell.find('input[type="file"]').on('change', function() {
-		    					 var reader = new FileReader();
-		    					 var f = this.files;
-		    					 $next_cell.find('.msg').text('Reading file from your device...');
-			    				 reader.onloadend = function () {
-			    					$next_cell.find('.msg').text('');
-			    					var encoded = reader.result;
-			    					var mime = encoded.substring("data:".length, encoded.indexOf(";base64"));
-			    					var mime_arr = mime.split('/');
-			    					if (mime_arr[0].toLowerCase() != 'video' || 'undefined' == typeof(mime_arr[1])) {
-			    						alert('Uploaded file was not a video. Please try again.');
-			    						return;
-			    					}
-			    					$next_cell.find('video').attr('src', encoded);
-			    					$next_cell.find('input[type="hidden"]').val(encoded);
-			    				 };
-			    				 setTimeout(function() {
-			    					reader.readAsDataURL(f[0]);
-		    					 }, 500);
-		    				}).trigger('click');	    					
-	    				} else {
-	    				*/
-	    		 			$next_cell.empty();
-		    				$next_cell.append('<input type="hidden" name="base64_string" value="" />');
-		    				$next_cell.append('<div style="margin-bottom:10px;"><button class="btn btn-success">Start recording</button><button class="btn btn-danger" style="display:none;">Stop recording</button></div>');
-		    				$next_cell.append('<video class="open-video" controls="" style="width:400px;display:none;"></video>');
-		    	            $next_cell.append('<div class="progress" style="max-width:100%;margin:0px auto 0px auto;"><div class="progress-bar" style="width:0%;" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div></div>');
-		    				$next_cell.append('<div class="progress-text"><span>0:00</span> / 1:00</div>')
-		    	            navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(function(camera) {
-		    	                var recordingHints = {
-		    	                    type: 'video',
-		    	                    /* mimeType: 'video/webm;codecs=h264', */
-		    	                    /* recorderType: MediaStreamRecorder */
-		    	                };
-		    	                recorder = RecordRTC(camera, recordingHints);  // Global
-		    	                var is_recording = false;
-    	                        $next_cell.find('video')[0].muted = false;
-    	                        $next_cell.find('video')[0].srcObject = null;
-	    	                	$next_cell.find('video').show();
-		    	                $next_cell.find('button:first').on('click', function() {
-		    	                	is_recording = true;
-		    	                	var $this = $(this);
-		    	                	$this.hide();
-		    	                	$this.nextAll('button').show();
-		    	                	recorder.startRecording();
-		    	                	var timer = 0;
-		    	                	var max_time = 60;
-		    	                	var doTimer = function(timer) {
-		    	                		if (!is_recording) return;
-		    	                		if (timer == max_time) {
-				    	                	is_recording = false;
-				    	                	$(this).hide();
-				    	                    recorder.stopRecording(function() {
-				    	                        var blob = recorder.getBlob();
-				    	                        $next_cell.find('video')[0].muted = false;
-				    	                        $next_cell.find('video')[0].srcObject = null;
-				    	                        $next_cell.find('video')[0].pause();
-				    	                        camera.getTracks().forEach(function(track) {
-				    	                            track.stop();
-				    	                        });
-				    	                        $next_cell.find('video')[0].src = URL.createObjectURL(blob);
-				    	                        $next_cell.find('video').show();
-				    	                        $next_cell.find('.progress, .progress-text').hide();
-				    	                        var reader = new FileReader();
-				    	                        reader.onloadend = function() {
-				    	                            var encoded = reader.result;                
-				    	                            $next_cell.find('input[type="hidden"]').val(encoded);
-				    	                        }
-				    	                        reader.readAsDataURL(blob); 
-				    	                    });
-		    	                			return;
-		    	                		}
-		    	                		timer++;
-		    	                		if (timer == max_time) {
-		    	                			$next_cell.find('button:last').trigger('click');
-		    	                			return;
-		    	                		}
-		    	                		var str = calculateTimeDuration(timer);
-		    	                		$this.closest('.row').find('.progress-text').find('span').text(str);
-		    	                		var width = (timer / max_time) * 100;
-		    	                		$this.closest('.row').find('.progress').children('div').css('width', width + '%');
-		    	                		setTimeout(function() {
-		    	                			doTimer(timer);
-		    	                		}, 1000);
-		    	                	}
-		    	                	doTimer(timer);
-		    	                });
-		    	                $next_cell.find('button:last').on('click', function() {
-		    	                	is_recording = false;
-		    	                	$(this).hide();
-		    	                    recorder.stopRecording(function() {
-		    	                        var blob = recorder.getBlob();
-		    	                        $next_cell.find('video')[0].muted = false;
-		    	                        $next_cell.find('video')[0].srcObject = null;
-		    	                        $next_cell.find('video')[0].pause();
-		    	                        camera.getTracks().forEach(function(track) {
-		    	                            track.stop();
-		    	                        });
-		    	                        $next_cell.find('video')[0].src = URL.createObjectURL(blob);
-		    	                        $next_cell.find('video').show();
-		    	                        $next_cell.find('.progress, .progress-text').hide();
-		    	                        var reader = new FileReader();
-		    	                        reader.onloadend = function() {
-		    	                            var encoded = reader.result;                
-		    	                            $next_cell.find('input[type="hidden"]').val(encoded);
-		    	                        }
-		    	                        reader.readAsDataURL(blob); 
-		    	                    });
-		    	                });
-		    	            });
-	    				// };
+
+	    	 			$next_cell.empty();
+	    				$next_cell.append('<input type="hidden" name="base64_string" value="" />');
+	    				$next_cell.append('<div style="margin-bottom:10px;"><button class="btn btn-success">Start recording</button><button class="btn btn-danger" style="display:none;">Stop recording</button></div>');
+	    				$next_cell.append('<video class="open-video" controls="false" style="width:100%;display:none;"></video>');
+	    	            $next_cell.append('<div class="progress" style="max-width:400px;margin:0px auto 0px auto;"><div class="progress-bar" style="width:0%;" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div></div>');
+	    				$next_cell.append('<div class="progress-text"><span>0:00</span> / 1:00</div>');
+	    	            navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(function(camera) {
+	    	                $next_cell.find('video')[0].srcObject = camera;
+	    	                $next_cell.find('video')[0].autoplay = true;
+	    	                $next_cell.find('video')[0].controls = false;
+	    	                $next_cell.find('video')[0].muted = true;
+	    	                $next_cell.find('video').show();
+	    	                recorder = RecordRTC(camera, {
+	    	                    type: 'video'
+	    	                });
+	    	                var is_recording = false;
+	    	                $next_cell.find('button:first').on('click', function() {
+	    	                	is_recording = true;
+	    	                	var $this = $(this);
+	    	                	$this.hide();
+	    	                	$this.nextAll('button').show();
+	    	                	recorder.startRecording();
+	    	                	var timer = 0;
+	    	                	var max_time = 60;
+	    	                	var doTimer = function(timer) {
+	    	                		if (!is_recording) return;
+	    	                		if (timer == max_time) {
+	    	    	                	is_recording = false;
+	    	    	                	$(this).hide();
+	    	    	                    recorder.stopRecording(function() {
+	    	    	                        var blob = recorder.getBlob();
+	    	    	                        camera.getTracks().forEach(function(track) {
+	    	    	                        	track.stop();
+	    	    	                        });
+	    	    	    	                $next_cell.find('video')[0].srcObject = null;
+	    	    	    	                $next_cell.find('video')[0].autoplay = false;
+	    	    	    	                $next_cell.find('video')[0].controls = true;
+	    	    	    	                $next_cell.find('video')[0].muted = false;
+	    	    	                        $next_cell.find('video')[0].src = URL.createObjectURL(blob);
+	    	    	                        $next_cell.find('.progress, .progress-text').hide();
+	    	    	                        var reader = new FileReader();
+	    	    	                        reader.onloadend = function() {
+	    	    	                            var encoded = reader.result;                
+	    	    	                            $next_cell.find('input[type="hidden"]').val(encoded);
+	    	    	                        }
+	    	    	                        reader.readAsDataURL(blob); 
+	    	    	                    });
+	    	                			return;
+	    	                		}
+	    	                		timer++;
+	    	                		var str = calculateTimeDuration(timer);
+	    	                		$this.closest('.row').find('.progress-text').find('span').text(str);
+	    	                		var width = (timer / max_time) * 100;
+	    	                		$this.closest('.row').find('.progress').children('div').css('width', width + '%');
+	    	                		setTimeout(function() {
+	    	                			doTimer(timer);
+	    	                		}, 1000);
+	    	                	}
+	    	                	doTimer(timer);
+	    	                });
+	    	                $next_cell.find('button:last').on('click', function() {
+	    	                	is_recording = false;
+	    	                	$(this).hide();
+	    	                    recorder.stopRecording(function() {
+	    	                        var blob = recorder.getBlob();
+	    	                        camera.getTracks().forEach(function(track) {
+	    	                        	track.stop();
+	    	                        });
+	    	    	                $next_cell.find('video')[0].srcObject = null;
+	    	    	                $next_cell.find('video')[0].autoplay = false;
+	    	    	                $next_cell.find('video')[0].controls = true;
+	    	    	                $next_cell.find('video')[0].muted = false;
+	    	                        $next_cell.find('video')[0].src = URL.createObjectURL(blob);
+	    	                        $next_cell.find('.progress, .progress-text').hide();
+	    	                        var reader = new FileReader();
+	    	                        reader.onloadend = function() {
+	    	                            var encoded = reader.result;                
+	    	                            $next_cell.find('input[type="hidden"]').val(encoded);
+	    	                        }
+	    	                        reader.readAsDataURL(blob); 
+	    	                    });
+	    	                });
+	    	            });	    				
+	    				
 	    	            break;
 	    		}
 	    		// Broadcast
